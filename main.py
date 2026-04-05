09b5414f9b8834121ca00922460923796d5f5914
mochinekoo <mochinekoo0113@gmail.com>
2026-04-05 21:25:28 +0900

    [add] テキストボックスを追加

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import tkinter
 from tkinter import filedialog
+from tkinter import Button
+from tkinter import Frame
 
 window = tkinter.Tk()
-window.wm_title("メモ帳")
 
 def openTextChooseDialog():
     fileSelectType = [("テキストファイル", ".txt")]
@@ -23,14 +24,23 @@ def runSaveCommand():
 def runExitCommand():
     sys.exit(0)
 
-menu = tkinter.Menu(window, tearoff=0)
-window.config(menu=menu)
-file_menu = tkinter.Menu(menu, tearoff=0)
-file_menu.add_command(label="開く", command=runOpenFileCommand)
-file_menu.add_command(label="保存する", command=runSaveCommand)
-file_menu.add_command(label="終了する", command=runExitCommand)
+def windowInit():
+    window.wm_title("メモ帳")
+    window.wm_geometry("1280x720")
 
-menu.add_cascade(label="ファイル", menu=file_menu)
+    menu = tkinter.Menu(window, tearoff=0)
+    window.config(menu=menu)
+    file_menu = tkinter.Menu(menu, tearoff=0)
+    file_menu.add_command(label="開く", command=runOpenFileCommand)
+    file_menu.add_command(label="保存する", command=runSaveCommand)
+    file_menu.add_command(label="終了する", command=runExitCommand)
+    menu.add_cascade(label="ファイル", menu=file_menu)
 
+    frame_1 = Frame(window, borderwidth=4)
+    frame_1.pack(side="left", fill="both", expand=True)
+    entry = tkinter.Text(frame_1)
+    entry.pack(side="left", fill="both", anchor="nw", expand=True)
 
-window.mainloop()
+if __name__ == "__main__":
+    windowInit()
+    window.mainloop()
