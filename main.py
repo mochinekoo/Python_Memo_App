06baae228d1305490adc6d62e6c6435599ed5865
mochinekoo <mochinekoo0113@gmail.com>
2026-04-06 12:19:45 +0900

    [add] ドキュメントを追加しました

--- main.py
+++ main.py
@@ -8,16 +8,29 @@ from tkinter import messagebox
 window = tkinter.Tk()
 
 def openTextChooseDialog():
+    """
+    開くファイルを選ぶ関数
+    :return: 選んだパス
+    """
+
     fileSelectType = [("テキストファイル", ".txt")]
     filePath = tkinter.filedialog.askopenfilename(filetypes=fileSelectType)
     return filePath
 
 def openTextSaveDialog():
+    """
+    ファイルを保存する場所を選ぶ関数
+    :return: 選んだパス
+    """
+
     fileSelectType = [("テキストファイル", ".txt")]
     savePath = tkinter.filedialog.asksaveasfilename(filetypes=fileSelectType)
     return savePath
 
 def runOpenFileCommand():
+    """
+    ファイルを開く関数（メニュー用）
+    """
     selectPath = openTextChooseDialog()
     if selectPath == "":
         print("何も選ばれませんでした")
@@ -25,6 +38,10 @@ def runOpenFileCommand():
         print(selectPath)
 
 def runSaveCommand():
+    """
+    ファイルを保存する関数（メニュー用）
+    """
+
     entryText = entry.get("1.0", "end")
     selectPath = openTextSaveDialog()
     if selectPath == "":
@@ -35,9 +52,17 @@ def runSaveCommand():
         tkinter.messagebox.showinfo(title="成功", message="正常にファイルが保存されました")
 
 def runExitCommand():
+    """
+    アプリを終了する関数（メニュー用）
+    """
     sys.exit(0)
 
 def windowInit():
+    """
+    ウインドウを設定する関数
+    :return: 成功した場合はTrueを返す
+    """
+
     global entry
     window.wm_title("メモ帳")
     window.wm_geometry("1280x720")
@@ -54,6 +79,7 @@ def windowInit():
     frame_1.pack(side="left", fill="both", expand=True)
     entry = tkinter.Text(frame_1)
     entry.pack(side="left", fill="both", anchor="nw", expand=True)
+    return True
 
 if __name__ == "__main__":
     windowInit()
